f679465dbd4eb041462a96ecde5c9420aec91fb7
Ben <benjipruv@gmail.com>
2025-03-10 15:36:37 +0100

    debut Kmean

---
+++ src/Kmeans/Kmean.java
@@ -0,0 +1,19 @@
+package Kmeans;
+
+import java.util.HashSet;
+
+
+abstract class Kmean<T>{
+    int k ;
+    HashSet<T> elts;
+    HashSet<T> centres;
+
+    abstract void initCentres();
+
+    abstract void 
+
+
+
+
+
+}
