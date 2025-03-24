486ac4afdb86647f422adb49cda559759e43e1ba
Ben <benjipruv@gmail.com>
2025-03-24 15:44:31 +0100

    KmeanSimple et JAVADOC

--- src/Formes/Point.java
+++ src/Formes/Point.java
@@ -1,6 +1,9 @@
 package Formes;
 
-/** Classe basique d'un point dans un espace en deux dimensions
+/**
+ * Classe représentant un point dans un espace à deux dimensions.
+ * Cette classe permet de créer des objets représentant des points avec des coordonnées x et y, ainsi qu'un groupe associé.
+ * Elle contient des méthodes pour obtenir et modifier les coordonnées du point, ainsi que des méthodes pour comparer deux points.
  */
 public class Point {
 
@@ -10,17 +13,22 @@ public class Point {
     public int groupe;
 
     /**
-     * Constructeur de base à 2 paramètres
-     * @param x float représentant la coordonnée x du point
-     * @param y float représentant la coordonnée y du point
+     * Constructeur de la classe Point avec deux paramètres de coordonnées.
+     * Ce constructeur initialise un point avec les coordonnées spécifiées x et y. Le groupe est initialisé à -1 par défaut.
+     * 
+     * @param x Coordonnée x du point.
+     * @param y Coordonnée y du point.
      */
-
     public Point(float x, float y){
         this.x = x;
         this.y = y;
         this.groupe = -1;
     }
 
+    /**
+     * Constructeur par défaut de la classe Point.
+     * Ce constructeur initialise un point avec les coordonnées (0, 0) et le groupe à -1 par défaut.
+     */
     Point(){
         this.x = 0;
         this.y = 0;
@@ -28,67 +36,107 @@ public class Point {
     }
 
     /**
-     * Getter de x
-     * @return la valeur de x
+     * Retourne la coordonnée x du point.
+     * 
+     * @return La valeur de la coordonnée x du point.
      */
-    public float getX(){return x;}
-
-    /**
-     * Getter de y
-     * @return la valeur de y
-     */
-    public float getY(){return y;}
-
-    public void setX(float x){this.x=x;}
-
-    public void setY(float y){this.y=y;}
-
-    /**
-     * Getter du groupe
-     * @return le label du groupe
-     */
-
-    /** Méthode qui renvoie la distance euclidienne entre notre point et un autre
-     *
-     * @param b : autre point avec lequel on calcule la distance de type Point
-     * @return : un float étant la distance euclidienne
-     */
-    public float distanceEuclidienne(Point b){
-        return (float) Math.sqrt(Math.pow(b.getX()-this.getX(),2)+Math.pow(b.getY()-this.getY(),2));
+    public float getX(){
+        return x;
     }
 
-    public int getGroupe(){ return this.groupe; }
-    public void setGroupe(int g){ this.groupe = g; }
-
-    @Override
-    public String toString(){ return "x: " + this.getX() + "| y: " + this.getY(); }
+    /**
+     * Retourne la coordonnée y du point.
+     * 
+     * @return La valeur de la coordonnée y du point.
+     */
+    public float getY(){
+        return y;
+    }
 
     /**
-     * Fait avec chat GPT, l'override du equals et du hashcode permet d'utiliser les méthodes contains et equals sur les HashSet<Point>
-     * et considère des points égaux si ils possèdent les mêmes coordonnées
+     * Modifie la coordonnée x du point.
+     * 
+     * @param x La nouvelle valeur de la coordonnée x.
+     */
+    public void setX(float x){
+        this.x = x;
+    }
+
+    /**
+     * Modifie la coordonnée y du point.
+     * 
+     * @param y La nouvelle valeur de la coordonnée y.
+     */
+    public void setY(float y){
+        this.y = y;
+    }
+
+    /**
+     * Retourne le groupe auquel le point appartient.
+     * 
+     * @return Le groupe du point.
+     */
+    public int getGroupe(){
+        return this.groupe;
+    }
+
+    /**
+     * Modifie le groupe du point.
+     * 
+     * @param g Le groupe à assigner au point.
+     */
+    public void setGroupe(int g){
+        this.groupe = g;
+    }
+
+    /**
+     * Retourne une représentation sous forme de chaîne du point.
+     * 
+     * @return Une chaîne représentant le point, avec ses coordonnées x et y.
+     */
+    @Override
+    public String toString(){
+        return "x: " + this.getX() + "| y: " + this.getY();
+    }
+
+    /**
+     * Redéfinition de la méthode equals pour comparer deux points.
+     * Cette méthode considère deux points égaux s'ils ont les mêmes coordonnées x et y.
+     * 
+     * @param obj L'objet à comparer avec l'instance courante.
+     * @return true si les deux points ont les mêmes coordonnées, sinon false.
      */
     @Override
     public boolean equals(Object obj){
+        if (this == obj) return true; // Vérification de la référence
+        if (obj == null || getClass() != obj.getClass()) return false; // Vérification du type
         Point p = (Point) obj;
         return p.x == this.x && p.y == this.y;
     }
 
+    /**
+     * Redéfinition de la méthode hashCode pour garantir une cohérence avec la méthode equals.
+     * Cette méthode calcule un code de hachage basé sur les coordonnées du point.
+     * Elle est nécessaire pour utiliser des méthodes comme contains et equals sur des HashSet<Point>.
+     * 
+     * @return Le code de hachage du point.
+     */
     @Override
     public int hashCode() {
         return 31 * Float.hashCode(x) + Float.hashCode(y);
     }
 
+    /**
+     * Méthode principale pour tester l'égalité de deux points.
+     * 
+     * @param args Les arguments de la ligne de commande (non utilisés ici).
+     */
     public static void main(String[] args){
         Point a = new Point(0,3);
         Point b = new Point(5,7);
         Point c = new Point(0,3);
-
-        System.out.println(a.distanceEuclidienne(b));
         
-        System.out.println(a.equals(b));
-        System.out.println(c.equals(a));
+        System.out.println(a.equals(b)); // Affiche false
+        System.out.println(c.equals(a)); // Affiche true
     }
-
-
-
 }
--- src/Kmeans/KMeanSimple.java
+++ src/Kmeans/KMeanSimple.java
@@ -5,49 +5,69 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
+/**
+ * Implémentation de l'algorithme des K-means pour un ensemble de points en 2D.
+ * Cette classe permet de regrouper les points en k clusters en utilisant la méthode des K-means.
+ * La distance utilisée pour le regroupement est la distance euclidienne.
+ * 
+ * @see KMean
+ * @see Point
+ */
 public class KMeanSimple extends KMean<Point>{
 
     /**
-     * Méthode renvoyant k points aléatoires dans la liste de points ("elts")
-     * qui seront utilisés comme les premiers centre de l'algorithme des KMeans
+     * Initialise les centres des clusters en sélectionnant aléatoirement k points parmi les éléments.
+     * Chaque centre est ajouté à l'ensemble des centres.
      */
     @Override
     protected void initialiserCentres(){
         int taille = this.elts.size();
-        /*Transformation en liste pour pouvoir accéder aux différents points par leur indice*/
+        /* Transformation en liste pour pouvoir accéder aux différents points par leur indice */
         ArrayList<Point> liste = new ArrayList<>(this.elts);
         Random random = new Random();
         int indice;
         int compteur = 0;
-        while(compteur<k){
-
+        while(compteur < k){
             indice = random.nextInt(taille);
             if (!this.centres.contains(liste.get(indice))){
                 Point p = liste.get(indice);
-                Point centre = new Point(p.getX(),p.getY());
+                Point centre = new Point(p.getX(), p.getY());
                 centre.setGroupe(compteur);
                 this.centres.add(centre);
-                compteur+=1;
-            }   
-            }  
-     }
+                compteur += 1;
+            }
+        }
+    }
 
-    /* Rempli les groupes avec leurs points les plus proches en utilisant la distance euclidienne
+    /**
+     * Calcule la distance euclidienne entre deux points.
+     *
+     * @param a Le premier point
+     * @param b Le second point
+     * @return La distance euclidienne entre les deux points
+     */
+    @Override
+    protected float distance(Point a, Point b){
+        return (float) Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2));
+    }
+
+    /**
+     * Met à jour les groupes en attribuant chaque point au centre le plus proche.
+     * Chaque point est ajouté au groupe de son centre le plus proche en fonction de la distance euclidienne.
      */
     @Override
     protected void MAJGroupes(){
-
         // Vidage des groupes avant de pouvoir les remplir
         for (int i = 0; i < k; i++) {
             groupes.get(i).clear();
         }
 
-        // Pour chaque point on recherche à quel centre il est le plus proche et on l'ajoute dans le groupe correspondant
+        // Pour chaque point, on recherche le centre le plus proche et on l'ajoute dans le groupe correspondant
         for(Point p: elts){
             float min = Float.POSITIVE_INFINITY;
             int meilleurGroupe = -1;
             for (Point centre : centres) {
-                float distance = p.distanceEuclidienne(centre);
+                float distance = this.distance(p, centre);
                 if (distance < min){
                     min = distance;
                     meilleurGroupe = centre.groupe;
@@ -58,7 +78,10 @@ public class KMeanSimple extends KMean<Point>{
     }
 
     /**
-     * Met à jour les centres et renvoie un booléen qui indique si les centres ont convergés, et donc indique si l'on est arrivé à la fin 
+     * Met à jour les centres des clusters en recalculant la moyenne des coordonnées des points de chaque groupe.
+     * La méthode retourne un booléen indiquant si les centres ont convergé, c'est-à-dire si le mouvement des centres est inférieur à un seuil.
+     *
+     * @return true si les centres ont bougé, false si la convergence est atteinte
      */
     @Override
     protected boolean MAJCentres(){
@@ -69,41 +92,79 @@ public class KMeanSimple extends KMean<Point>{
             float taille = liste.size();
             float newX = 0;
             float newY = 0;
-            for(Point p:liste){
+            for(Point p: liste){
                 newX += p.getX();
                 newY += p.getY();
             }
-            newX = newX/taille; newY = newY/taille;
-            Point newCentre = new Point(newX,newY);
-            if(centre.distanceEuclidienne(newCentre)>=SEUIL_CONVERGENCE){
+            newX = newX / taille; 
+            newY = newY / taille;
+            Point newCentre = new Point(newX, newY);
+            if(this.distance(centre, newCentre) >= SEUIL_CONVERGENCE){
                 continuer = true;
-                centre = newCentre;
+                centre.setX(newX);
+                centre.setY(newY);
             }
         }
         return continuer;
     }
 
-    
-    @Override
-    protected void calculer(){ return; }
+    /**
+     * Exécute l'algorithme des K-means jusqu'à convergence. À chaque itération, les groupes sont mis à jour, puis les centres.
+     */
+    public void executerKMeans() {
+        boolean continuer = true;
+        while (continuer) {
+            MAJGroupes();   // Mise à jour des groupes
+            continuer = MAJCentres();  // Mise à jour des centres et vérification de la convergence
+        }
+    }
 
+    /**
+     * Calcule une itération de l'algorithme K-means en mettant à jour les groupes et les centres.
+     *
+     * @param continuer Un booléen qui indique si l'algorithme doit continuer (si les centres n'ont pas convergé).
+     * @return true si les centres doivent encore être mis à jour, false si l'algorithme a convergé
+     */
+    @Override
+    protected boolean calculer(boolean continuer){ 
+        if (continuer) {
+            MAJGroupes();
+            continuer = MAJCentres();
+        }
+        return continuer;
+    }
+
+    /**
+     * Constructeur de la classe KMeanSimple avec un nombre k de clusters et un ensemble d'éléments.
+     *
+     * @param k Le nombre de clusters à créer
+     * @param elements L'ensemble de points à regrouper
+     */
     public KMeanSimple(int k, HashSet<Point> elements){
         super(k, elements);
     }
 
+    /**
+     * Constructeur par défaut qui initialise k à 2 et crée un ensemble vide de points.
+     */
     public KMeanSimple(){
         super(2, new HashSet<Point>());
     }
 
-
-
+    /**
+     * Point d'entrée pour tester l'algorithme K-means avec des exemples de points.
+     *
+     * @param args Arguments de la ligne de commande (non utilisés)
+     */
     public static void main(String[] args) {
-        Point a = new Point(0,3);
-        Point b = new Point(5,7);
-        Point c = new Point(6,6);
-        Point d = new Point(7,7);
-        Point e = new Point(8,8);
-        Point f = new Point(9,9);
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+        Point c = new Point(3, 1);
+        Point d = new Point(6, 5);
+        Point e = new Point(7, 6);
+        Point f = new Point(8, 5);
+        Point g = new Point(9, 8);
+        Point h = new Point(10, 7);
 
         HashSet<Point> elts = new HashSet<>();
 
@@ -113,8 +174,10 @@ public class KMeanSimple extends KMean<Point>{
         elts.add(d);
         elts.add(e);
         elts.add(f);
+        elts.add(g);
+        elts.add(h);
 
-        KMeanSimple test = new KMeanSimple(3,elts);
+        KMeanSimple test = new KMeanSimple(3, elts);
 
         System.out.println("Liste des centres :");
         for(Point p:test.centres){
@@ -123,14 +186,29 @@ public class KMeanSimple extends KMean<Point>{
 
         test.MAJGroupes();
 
-        for(int i=0;i<test.k;i++){
+        for(int i = 0; i < test.k; i++){
             HashSet<Point> liste = test.groupes.get(i);
-            System.out.printf("Groupe %d%n",i);
+            System.out.printf("Groupe %d%n", i);
             for (Point p: liste){
                 System.out.println(p);
             }
         }
 
+        KMeanSimple test1 = new KMeanSimple(3, elts);
+        test1.executerKMeans();
+
+        System.out.println("Liste des centres :");
+        for(Point p:test1.centres){
+            System.out.println(p);
+        }
+        System.err.println("");
+
+        for(int i = 0; i < test1.k; i++){
+            HashSet<Point> liste = test1.groupes.get(i);
+            System.out.printf("Groupe %d%n", i);
+            for (Point p: liste){
+                System.out.println(p);
+            }
+        }
     }
 }
-
