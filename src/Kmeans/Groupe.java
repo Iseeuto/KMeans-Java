8f8e246e31b7868bdbaaffdd39612364538664e8
Iseeuto <reporttanguy@gmail.com>
2025-04-02 15:31:10 +0200

    Ajouté javadoc pour Groupe

--- src/Kmeans/Groupe.java
+++ src/Kmeans/Groupe.java
@@ -7,22 +7,42 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
+/**
+ * Classe representant un groupe de points dans un algorithme de clustering K-Means.
+ * Chaque groupe est associé à une couleur unique et contient une liste de points.
+ */
 public class Groupe {
 
+    /**
+     * Liste des points appartenant à ce groupe.
+     */
     public ArrayList<Point> points = new ArrayList<>();
 
+    /**
+     * Couleur associée au groupe pour la visualisation.
+     */
     public Color couleur;
 
-    @Override
-    public String toString(){
-        String str = "";
-        for(Point p: this.points) str += p.toString() + "\n";
-        return str;
-    }
-
-    public Groupe(){
+    /**
+     * Constructeur de la classe {@code Groupe}.
+     * Initialise une couleur aléatoire pour le groupe.
+     */
+    public Groupe() {
         Random rand = new Random();
         this.couleur = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
     }
 
+    /**
+     * Retourne une représentation textuelle du groupe en listant ses points.
+     *
+     * @return Une chaîne de caractères contenant les coordonnées des points du groupe.
+     */
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (Point p : this.points) {
+            str.append(p.toString()).append("\n");
+        }
+        return str.toString();
+    }
 }
